586d8115fd62d82daee3bd88f0fa69c1eeb169cb
Emre <emreergun@bgnmobi.com>
2021-05-01 10:12:51 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/emreergun/flutter_http_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.emreergun.flutter_http_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_http_app/model/UserModel.dart';
+import 'package:http/http.dart' as http;
+import 'dart:developer' as developer;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+Future<UserModel> createUser(String name, String jobTitle) async {
+  final String apiUrl = "https://reqres.in/api/users";
+  var url = Uri.parse(
+      apiUrl); // Uri tipinde değer aldığı için convert işlemi uygulandı
+
+  // Request -> Response
+  final response = await http.post(url, body: {
+    "name": name,
+    "job": jobTitle,
+  });
+
+  // if status correct
+  if (response.statusCode == 201) {
+    final String responseString = response.body;
+    return userModelFromJson(responseString);
+  } else {
+    return null;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  UserModel _user;
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController jobController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+            ),
+            TextField(
+              controller: jobController,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            // control
+            _user == null
+                ? Container() // _user null ise
+                : Text( // _user null değil ise
+                    "The User ${_user.name} , created At: ${_user.createdAt.toIso8601String()}"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final String name = nameController.text;
+          final String jobTitle = jobController.text;
+          final UserModel user = await createUser(name, jobTitle);
+
+          setState(() {
+            _user = user;
+          });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
