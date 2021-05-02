528f61819b26a23bcbc808f918cca0a124588e56
Emre <emreergun@bgnmobi.com>
2021-05-02 08:41:45 +0300

    Change UserData Screen Stateful => Stateless

--- lib/screens/userdata/UserDataScreen.dart
+++ lib/screens/userdata/UserDataScreen.dart
@@ -2,28 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_http_app/model/UserData.dart';
 import 'package:provider/provider.dart';
 
-class UserDataScreen extends StatefulWidget {
+class UserDataScreen extends StatelessWidget {
   static String routeName = "/user_data"; // Router Name
-
-  @override
-  _UserDataScreenState createState() => _UserDataScreenState();
-}
-
-class _UserDataScreenState extends State<UserDataScreen> {
   @override
   Widget build(BuildContext context) {
-    UserData userData =Provider.of<UserData>(context);
+    UserData userData = Provider.of<UserData>(context);
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(userData.getEmail),
-            Text(userData.getPassword),
-          ],
-        ),
-      )
-    );
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(userData.getEmail),
+          Text(userData.getPassword),
+        ],
+      ),
+    ));
   }
 }
